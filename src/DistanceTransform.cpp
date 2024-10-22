dacf59a788bcd0f078398fe6080004c1c18f559a
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-10-22 14:59:20 +0100

    distrans inprogress

--- src/DistanceTransform.cpp
+++ src/DistanceTransform.cpp
@@ -25,25 +25,38 @@ int main() {
 		{ -1, -1 }, { 0, -1 }, { 1, -1 },
 		{ -1, 0 }
 	};
+	
+	vector<vector<int>> disTr;
+ 
+	int min, minIdx, val, v;
+	float rootTwo = sqrtf(2);
 
-	int min, val, v;
+	for (int i = 0; i < src.rows; i++) {
+		for (int j = 0; j < src.cols; j++) {
+			if (disTrans.at<uchar>(i, j) == 255) disTrans.at<uchar>(i, j) = INT32_MAX;
+		}
+	}
 
 	for (int i = 0; i < src.rows; i++) {
 		for (int j = 0; j < src.cols; j++) {
 			
 			val = disTrans.at<uchar>(i, j);
-			if (val != 0) continue; // already visited + ignore bg
+			if (val == INT32_MAX) continue; // already visited + ignore bg
+			
 			min = 0;
-
-			for (Point& p : topLeft) {
-				int x = p.x + i; int y = p.y + i;
+			minIdx = -1;
+			for (int k = 0; k < topLeft.size(); k++) {
+				int x = topLeft[k].x + i; int y = topLeft[k].y + j;
 				if (x < 0 || x >= src.rows || y < 0 || y >= src.cols) continue;
 
-				val = disTrans.at<uchar>(x, y);
-
-
-				
+				if (disTrans.at<uchar>(x, y) < min) {
+					min = disTrans.at<uchar>(x, y);
+					minIdx = i;
+				}
 			}
+
+			if (minIdx == 0 || minIdx == 2) val += rootTwo; // This is diagonal pts' index from TopLeft
+			else val += 1;
 		}
 	}
 
@@ -57,12 +70,30 @@ int main() {
 
 	for (int i = src.rows - 1; i >= 0; i--) {
 		for (int j = src.cols - 1; j >= 0; j--) {
+			val = disTrans.at<uchar>(i, j);
+
+			if (val == INT32_MAX) continue;
+			min = 0;
+			minIdx = -1;
+			for (int k = 0; k < bottomRight.size(); k++) {
+				int x = bottomRight[k].x + i; int y = bottomRight[k].y + j;
+				if (x < 0 || x >= src.rows || y < 0 || y >= src.cols) continue;
+
+				if (disTrans.at<uchar>(x, y) < min) {
+					min = disTrans.at<uchar>(x, y);
+					minIdx = i;
+				}
+			}
+
+			if (minIdx == 0 || minIdx == 2) val += rootTwo; // This is diagonal pts' index from TopLeft
+			else val += 1;
 
 		}
 	}
 
+	cout << disTrans;
 
-
+	imshow("disTrans", disTrans);
 	
 	
 	//dilate(openOut, dilatedOut, struc, Point(-1, -1), 3);
